d9c5cd30258775c2feea67396da28c2ad651155b
szikers <kerszi@wp.pl>
2021-04-19 22:42:08 +0200

    - ulepszono zgrywanie numerow - stworzono nowy plik numery - petla do scrapowania numerow

--- ficda.py
+++ ficda.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 import re
 import json
+from time import sleep
+from random import randint
 
-
-MAIN_LINK = 'https://www.cdaction.pl/magazyn/'
+#MAIN_LINK = 'https://www.cdaction.pl/magazyn/'
 MAIN_LINK_1NR = 'https://www.cdaction.pl/magazyn/numer-2-30.html'
 MAIN_LINK_241NR = 'https://www.cdaction.pl/magazyn/numer-241-1.html'
 #https://www.cdaction.pl/magazyn/numer-241-1.html
@@ -17,30 +18,39 @@ CALY_LINK = 'file:///mnt/d/moje programy i inne/GNU/ficda/przykladowa_strona.htm
 
 
 
-
-
-
 class MyScrap:
     CHAPTERS = {
     'CHAPTER_GAME1':'ZA PIĘĆ DWUNASTA',
-    'CHAPTER_GAME2':'W PRODUKCJI',
-    'CHAPTER_GAME3':'RECENZJE',
-    'CHAPTER_GAME4':'KASZANKA ZONE'
+    'CHAPTER_GAME2':'ZA 5 12',
+    'CHAPTER_GAME3':'W PRODUKCJI',
+    'CHAPTER_GAME4':'RECENZJE',
+    'CHAPTER_GAME5':'KASZANKA ZONE',
+    'CHAPTER_GAME6':'KASZANKA',
+    'CHAPTER_GAME7':'JUŻ GRALIŚMY',
+    
     }
 
     H2_TAG="h2"
     DIV_TAG="div"
     CLASS_TAG1={"class":"blok_info"}
     CLASS_TAG2={"class":"tytul"}
-    
+
+    MAIN_LINK = 'https://www.cdaction.pl/magazyn/'
 
     site =''
     page =''    
     section_title=[]
     nr = ''
+    scraped = False
 
 
-    def __init__ (self, link):
+    def __init__ (self, curr_nr:int, biggest_nr=244):
+        if curr_nr>biggest_nr:
+            print ("Za duzy numer - ",curr_nr)
+            exit()
+        section_title=[]
+        nr_page=(biggest_nr/8)+1-(curr_nr/8)
+        link=self.MAIN_LINK+'numer-'+str(curr_nr)+'-'+str(int(nr_page))+'.html'              
         self.site=link
         body = urlopen(self.site) 
         soup = bs(body,'html.parser')
@@ -51,56 +61,48 @@ class MyScrap:
         self.nr = re.search('(https://)(www.cdaction.pl/)(magazyn/)(numer-[0-9]*)(-.*)(.html)',self.site)[4]
         
 
-
     def scrap (self):        
+        self.section_title=[]
         for chapter_names in self.CHAPTERS.values():
             for i in self.page:
-                a=i.find(self.H2_TAG).text
+                a=i.find(self.H2_TAG).text.strip()                                
                 if (a==chapter_names):
                     b=i.find_all(self.DIV_TAG,self.CLASS_TAG2)
                     for j in b:
                         self.section_title.append({self.nr:{chapter_names:j.text}})
 
+        self.scraped=True
         return self.section_title
-    
+
+
+
     def json_dumping (self):
-        #sprawdzic czy bylo zgrane do pamieci        
-        file = self.nr + '.json'
-        try:
-            with open(file, 'w') as f:
-                json.dump(self.section_title,f)
-        except ValueError as e:                
-            print ("Problem z plikiem "+file+" lub formatem json")
-            print (e)        
-            exit()
+        if self.scraped:
+            file = 'numery/'+self.nr + '.json'
+            try:
+                with open(file, 'w') as f:
+                    json.dump(self.section_title,f)             
+                status="ok"
+                if len(self.section_title)==0:
+                    status="pusty"
+                print ("Zgralem: "+file+" "+status)   
+            except ValueError as e:                
+                print ("Problem z plikiem "+file+" lub formatem json")
+                print (e)        
+                exit()
 
 
 
 #a=MyScrap(CALY_LINK)
-a=MyScrap(MAIN_LINK_241NR)
-
-a.scrap()
-a.json_dumping()
-
-exit ()
-print (b)
-a="numer-141"
-b=a+'.json'
-
-print (b)
 
 
-#a='https://www.cdaction.pl/magazyn/numer-1-30.html'
+#---zgrywamy numery do formatu json
 
-
-#print (re.search('(https://)(www.cdaction.pl/)(magazyn/)(numer-[0-9]?)(-.*)(.html)',a)[4])
-#numer-131-1.html
-
-#section_title=[{"nr":{"chap1":"nr"}},{"nr":{"chap1":"nr"}}]
-
-
-#print (section_title)
-#json.dump()        
+for i in range(244,245):
+    a=MyScrap(i)
+    a.scrap()
+    a.json_dumping()    
+    sleep (randint(1,2))
 
 
 
